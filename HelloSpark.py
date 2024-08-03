fd39aab9849483029d7ab57730f515db2f4d6546
cukri <sugier.michal26@gmail.com>
2024-08-04 00:18:17 +0200

    1st Spark Project

---
+++ HelloSpark.py
@@ -0,0 +1,28 @@
+import sys
+
+from pyspark.sql import *
+
+from lib.logger import Log4J
+from lib.utils import get_spark_app_config, load_survey_df
+
+if __name__ == "__main__":
+    conf = get_spark_app_config()
+    spark = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    logger = Log4J(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
+    logger.info("Starting HelloSpark")
+    #code
+    #conf_out = spark.sparkContext.getConf()
+    #logger.info(conf_out.toDebugString())
+    survey_df = load_survey_df(spark, sys.argv[1])
+    survey_df.show()
+
+    logger.info("Finished HelloSpark")
+    spark.stop()

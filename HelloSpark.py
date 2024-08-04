d52e08a2f51bf2f185d5aa75ac89a7d9b45b0a72
cukri <sugier.michal26@gmail.com>
2024-08-04 15:44:13 +0200

    Commit with tests

--- HelloSpark.py
+++ HelloSpark.py
@@ -3,7 +3,7 @@ import sys
 from pyspark.sql import *
 
 from lib.logger import Log4J
-from lib.utils import get_spark_app_config, load_survey_df
+from lib.utils import get_spark_app_config, load_survey_df, count_by_country
 
 if __name__ == "__main__":
     conf = get_spark_app_config()
@@ -22,7 +22,12 @@ if __name__ == "__main__":
     #conf_out = spark.sparkContext.getConf()
     #logger.info(conf_out.toDebugString())
     survey_df = load_survey_df(spark, sys.argv[1])
-    survey_df.show()
+    partitioned_survey_df = survey_df.repartition(2)
 
+    count_df = count_by_country(partitioned_survey_df)
+
+    logger.info(count_df.collect())
+
+    input("press enter")
     logger.info("Finished HelloSpark")
     spark.stop()
--- lib/utils.py
+++ lib/utils.py
@@ -15,4 +15,11 @@ def load_survey_df(spark, data_file):
     return spark.read \
         .option("header", "true")\
         .option("inferSchema", "true")\
-        .csv(sys.argv[1])
+        .csv(data_file)
+
+def count_by_country(survey_df):
+    return survey_df \
+        .where("Age < 40") \
+        .select("Age", "Gender", "Country", "state")\
+        .groupBy("Country") \
+        .count()
